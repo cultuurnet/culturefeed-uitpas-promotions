c1562119a52fc1ee7a4645c113f87af6286885b4
Stijn Swaanen <swaanenstijn@gmail.com>
2018-01-30 12:40:34 +0100

    Theme the template file

---
+++ theme/culturefeed-uitpas-promotions-search.tpl.php
@@ -0,0 +1,16 @@
+<div id="uitpas-search"
+     data-card-systems-checked="<?php print $card_systems_string; ?>"
+     data-card-systems-visible="<?php print $filter_cardsystem; ?>"
+     data-elastic-search-url="<?php print $url_uitpas; ?>"
+     data-fuzzy-search="<?php print $fuzzy_search; ?>"
+     data-last-chance-weeks="<?php print $last_chance; ?>"
+     data-search-placeholder-text="<?php print $placeholder; ?>"
+     data-uit-databank-url-prefix="<?php print $url_uitdatabank; ?>"
+     data-in-spotlight-sticky="<?php print $spotlight; ?>"
+     data-embedded="true"
+     data-show-active-advantages="true"
+     data-show-permanent-card-system-advantages="true"
+     data-show-published-advantages="false"
+     data-default-thumb-url="<?php print $thumb; ?>">
+</div>
+<script type="text/javascript" src="<?php print $jsscript; ?>"></script>
